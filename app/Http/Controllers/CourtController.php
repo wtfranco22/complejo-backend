7b32d3b3a1828d902591ef16a9f1c433bdcc44d8
wtfranco22 <francofranco_96@hotmail.com>
2023-04-06 04:12:40 -0300

    agregando comentarios

--- app/Http/Controllers/CourtController.php
+++ app/Http/Controllers/CourtController.php
@@ -10,10 +10,12 @@ use function PHPUnit\Framework\isNull;
 class CourtController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Listado de todas las canchas
+     * @return Response
      */
     public function index()
     {
+        // $courts es la coleccion de elementos Court
         $courts = Court::all();
         return response()->json([
             'status' => 1,
@@ -23,15 +25,19 @@ class CourtController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Agregamos nueva cancha para alquilar
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
+        // $court es un elemento Court que contiene los datos de la nueva cancha para jugar
         $request->validate([
             'name' => 'required',
             'description' => 'required'
         ]);
         if (isset($request->active_at) || isset($request->inactive_at)) {
+            // si una hora es declarada, ambas deben serlo
             if (!(isset($request->active_at) && isset($request->inactive_at))) {
                 return response()->json([
                     'status' => 0,
@@ -40,11 +46,11 @@ class CourtController extends Controller
             }
         }
         $court = new Court();
-        $court->active = isset($request->active) ? $request->active : false;
+        $court->active = isset($request->active) ? $request->active : false; //por defecto agregamos false
         $court->name = $request->name;
-        $court->active_at = isset($request->active_at) ? $request->active_at : null;
-        $court->inactive_at = isset($request->inactive_at) ? $request->inactive_at : null;
-        $court->image_url = isset($request->image_url) ? $request->image_url : null;
+        $court->active_at = isset($request->active_at) ? $request->active_at : null; //por defecto agregamos null
+        $court->inactive_at = isset($request->inactive_at) ? $request->inactive_at : null; //por defecto agregamos null
+        $court->image_url = isset($request->image_url) ? $request->image_url : null; //por defecto agregamos null
         $court->description = $request->description;
         $court->save();
         return response()->json([
@@ -54,10 +60,16 @@ class CourtController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Mostramos todos los datos que tiene la cancha particularmente
+     * @param int $id
+     * @return Response
      */
     public function show($id)
     {
+        /**
+         * $court es un elemento Court encontrado por su id $id
+         * $data es el mensaje informativo del proceso para el cliente
+         */
         $court = Court::find($id);
         if (isset($court->id)) {
             $data = [
@@ -75,14 +87,22 @@ class CourtController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Actualizamos los datos como estado y/o cambio automatico declarando horas y/o descripcion y/o imagen
+     * @param  Request $id
+     * @param int $id
+     * @return Response
      */
     public function update(Request $request, $id)
     {
+        /**
+         * $court es un elemento Court encontrado por su id $id
+         * $data es el mensaje informativo del proceso para el cliente
+         */
         $court = Court::find($id);
         if (isset($court->$id)) {
             $data = null;
             if (isset($request->active_at) || isset($request->inactive_at)) {
+                // si una hora es declarada, ambas deben serlo
                 if (isset($request->active_at) && isset($request->inactive_at)) {
                     $court->active_at = $request->active_at;
                     $court->inactive_at = $request->inactive_at;
@@ -94,6 +114,7 @@ class CourtController extends Controller
                 }
             }
             if (isNull($data)) {
+                // verificamos que la actualizacion siga siendo correcta
                 $court->active = (isset($request->active)) ? $request->active : $court->active;
                 $court->name = (isset($request->name)) ? $request->name : $court->name;
                 $court->image_url = (isset($request->image_url)) ? $request->image_url : $court->image_url;
@@ -104,28 +125,6 @@ class CourtController extends Controller
                     'msg' => '¡Datos de la cancha actualizada!'
                 ];
             }
-        } else {
-            $data = [
-                'status' => 0,
-                'msg' => 'Error, cancha no encontrada'
-            ];
-        }
-        return response()->json($data);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        $court = Court::find($id);
-        if (isset($court->id)) {
-            $court->active = false;
-            $court->save();
-            $data = [
-                'status' => 1,
-                'msg' => '¡Cancha deshabilitada!'
-            ];
         } else {
             $data = [
                 'status' => 0,
