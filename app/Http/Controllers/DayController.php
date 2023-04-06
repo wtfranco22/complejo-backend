170dafd8f2d4b022a864d78bd69150877de79cbb
wtfranco22 <francofranco_96@hotmail.com>
2023-04-06 04:10:23 -0300

    valida el dia ingresado, si esta abierto y devuelve en un formato para trabajar la fecha

--- app/Http/Controllers/DayController.php
+++ app/Http/Controllers/DayController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Day;
+use DateTime;
 use Illuminate\Http\Request;
 
 class DayController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Listado de los 7 dias de la semana
+     * @return Response
      */
     public function index()
     {
+        // $days es la coleccion de elementos Day
         $days = Day::all();
         return response()->json([
             'status' => 1,
@@ -21,10 +24,17 @@ class DayController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Actualizamos Day, cambiamos su estado activo y/o descripcion sobre el dia
+     * @param Resquest $request
+     * @param int $id
+     * @return Response
      */
     public function update(Request $request, $id)
     {
+        /**
+         * $day es un elemento Day encontrado por su id $id
+         * $data es el mensaje informativo del proceso para el cliente
+         */
         $day = Day::find($id);
         if (isset($day->id)) {
             $day->active = isset($request->active) ? $request->active : $day->active;
@@ -44,29 +54,55 @@ class DayController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Verificamos que el dia ingresado no tenga inconveniente para la reserva del turno
+     * @param String $dayUser
+     * @return Array
      */
-    public function destroy($id)
+    public function validateDay($dayUser)
     {
-        $day = Day::find($id);
-        if (isset($day->id)) {
-            $day->active = false;
-            $day->save();
-            $data = [
-                'status' => 1,
-                'msg' => '¡Día deshabilitado!'
-            ];
+        /**
+         * $day es el dia con otro formato del turno ingresado por el usuario
+         * $dateTimeNow es el horario del momento que se va a realizar la consulta
+         * $now es el horario en este momento pero en string para comparar con el dia ingresado por el usuario
+         * $numberDay es el dia (1 al 7) de la semana
+         * $workDay es el elemento Day que segun el dia ingresado verifica si esta activo (trabaja)
+         * $data es el mensaje informativo del proceso para el cliente
+         */
+        $day = DateTime::createFromFormat('d-m-Y H:i:s', $dayUser . ' 23:59:59');
+        if ($day !== false) {
+            // verificamos si existe la fecha
+            // $dateTimeNow = date('d-m-Y H:i:s');
+            $now = DateTime::createFromFormat('d-m-Y H:i:s', date('d-m-Y H:i:s'));
+            if (!($now->diff($day))->invert) {
+                // verificamos si el dia y hora son proximas y no una fecha atrasada
+                $numberDay = $day->format('N');
+                $workDay = Day::find($numberDay);
+                if ($workDay->active) {
+                    // verificamos si el dia ingresado esta abierto
+                    $data = [
+                        'valido' => true,
+                        'date' => $day
+                    ];
+                } else {
+                    $data = [
+                        'valido' => false,
+                        'msg' => 'No esta abierto',
+                        'date' => $day
+                    ];
+                }
+            } else {
+                $data = [
+                    'valido' => false,
+                    'msg' => 'La fecha ingresada ya paso',
+                    'date' => $day
+                ];
+            }
         } else {
             $data = [
-                'status' => 0,
-                'msg' => 'Error, dia no encontrado'
+                'valido'  => false,
+                'msg' => 'fecha incorrecta'
             ];
         }
-        return response()->json($data);
-    }
-
-    public function workday($id)
-    {
-        return Day::find($id)->active;
+        return $data;
     }
 }
